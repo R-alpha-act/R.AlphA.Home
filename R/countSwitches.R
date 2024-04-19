58c990ef4ecc70dd7bb05d4182fafb3fca1a3060
Raphaël <r.alpha.act@outlook.fr>
2024-04-19 17:35:39 +0200

    foldAllBr - countSwitches - get rid of accumulate, too slow

--- R/countSwitches.R
+++ R/countSwitches.R
@@ -11,32 +11,100 @@
 #' @importFrom stats runif
 #' @export
 
-countSwitches <- function(sttEnd, sttMark, endMark,
+countSwitches <- function(data, colNm, sttMark, endMark,
 							  includeStt = TRUE, includeEnd = TRUE){
-	# sttMark <- "start"
-	# endMark <- "end"
-	# includeStt <- TRUE
-	# includeEnd <- TRUE
-	grpTable <- sttEnd %>%
-		as.data.table %>%
-		rename(stepStr = ".") %>%
+	manualrun <- T
+	manualrun <- F
+	if (manualrun) {
+		warning("! parameters manually defined inside function 'countSwitches' for tests. Do not use results !")
+		sttMark <- "+"
+		endMark <- "-"
+		includeStt <- TRUE
+		includeEnd <- TRUE
+		# sttEnd <- docContentRet$brTag
+		data <- docContentRet
+		colNm <- "brTag"
+	}
+
+	fill_in <- function(prev, new, check) {
+		if_else(check == -1, 0, prev + check)
+	}
+
+	# dataX <- data
+	# dataX <- dataX %>% bind_rows(dataX, dataX, dataX, dataX, dataX) %>% print
+	# data <- dataX %>% slice(1:1E4)
+	# data %>%
+	# 	slice(1:10) %>%
+	# 	mutate(test = get(colNm))
+
+	grpTable <- data %>%
+		# mutate(stepStr = colValues) %>%
+		mutate(stepStr = get(colNm)) %>%
+		as_tibble %>%
+		# as.data.table %>%
+		# rename(stepStr = ".") %>%
+		# mutate(stepStr = brTag) %>%
 		mutate(findStt = stepStr == sttMark) %>%
 		mutate(findEnd = stepStr == endMark) %>%
 		mutate(nbStt = cumsum(findStt %>% replace_na(0))) %>%
 		mutate(nbEnd = cumsum(findEnd %>% replace_na(0))) %>%
-		# mutate(nbEnd = cumsum(stepStr == endMark) %>% replace_na(0)) %>%
-		mutate(grp = ifelse(nbStt!=nbEnd, nbStt, 0)) %>%
-		mutate(grpRetStt = ifelse(stepStr==sttMark & includeStt, nbStt,0)) %>%
-		mutate(grpRetEnd = ifelse(stepStr==endMark & includeEnd, nbStt,0)) %>%
-		# identity %>% print %>%
+		mutate(catLvl = nbStt - nbEnd) %>%
+		select(-nbStt, -nbEnd) %>%
+		# spot starts
+		mutate(inc1 = findStt & catLvl == 1) %>%
+		mutate(inc2 = findStt & catLvl == 2) %>%
+		mutate(inc3 = findStt & catLvl == 3) %>%
+		# spot when under category to reset
+		mutate(check1 = ifelse(catLvl >= (1-1), inc1, -1)) %>%
+		mutate(check2 = ifelse(catLvl >= (2-1), inc2, -1)) %>%
+		mutate(check3 = ifelse(catLvl >= (3-1), inc3, -1)) %>%
+		select(-opBr, -clBr, anyBr) %>%
+		# # initialize brut
+		# mutate(brut1 = inc1) %>%
+		# mutate(brut2 = inc2) %>%
+		# mutate(brut3 = inc3) %>%
+		# # slowest part : accumulate
+		# mutate(brut1 = accumulate2(brut1, check1 %>% tail(-1), fill_in)) %>%
+		# mutate(brut2 = accumulate2(brut2, check2 %>% tail(-1), fill_in)) %>%
+		# mutate(brut3 = accumulate2(brut3, check3 %>% tail(-1), fill_in)) %>%
+		# mutate(ret1 = ifelse(catLvl >= 1, brut1, 0)) %>%
+		# mutate(ret2 = ifelse(catLvl >= 2, brut2, 0)) %>%
+		# mutate(ret3 = ifelse(catLvl >= 3, brut3, 0)) %>%
+		#
+
+		mutate(tst1 = cumsum(inc1)) %>%
+		group_by(tst1) %>% mutate(tst2 = cumsum(inc2)) %>%
+		group_by(tst1, tst2) %>% mutate(tst3 = cumsum(inc3)) %>%
+		mutate(ret1 = ifelse(catLvl >= 1, tst1, 0)) %>%
+		mutate(ret2 = ifelse(catLvl >= 2, tst2, 0)) %>%
+		mutate(ret3 = ifelse(catLvl >= 3, tst3, 0)) %>%
+		# select(-content) %>%
 		identity
-	grpTable[stepStr==sttMark, grp:= grpRetStt]
-	grpTable[stepStr==endMark, grp:= grpRetEnd]
+
+
+	# grpTable %>%
+	# 	filter(rTs1 != ret1|rTs2 != ret2|rTs3 != ret3)
+	# 	filter(tst1 != ret1)
+
+		# mutate(inc3 = findStt & catLvl == 3) %>%
+		# mutate(inc4 = findStt & catLvl == 4) %>%
+		# mutate(nbEnd = cumsum(stepStr == endMark) %>% replace_na(0)) %>%
+		# mutate(grp = ifelse(nbStt!=nbEnd, nbStt, 0)) %>%
+		# mutate(grpRetStt = ifelse(stepStr==sttMark & includeStt, nbStt,0)) %>%
+		# mutate(grpRetEnd = ifelse(stepStr==endMark & includeEnd, nbStt,0)) %>%
+		# as_tibble %>% print(n = 130)
+		# identity
+	# grpTable[stepStr==sttMark, grp:= grpRetStt]
+	# grpTable[stepStr==endMark, grp:= grpRetEnd]
 	# print(grpTable)
 
-	return(grpTable$grp)
+	return(grpTable)
 }
-
+data.table(x = c(rep(1,1E7), rep(2, 3))) %>%
+	mutate(across(x, as.numeric)) %>%
+	group_by(x) %>%
+	mutate(tst = cumsum(x))
+	# mutate(tst = ifelse(x == 10, 0, cumsum(x)))
 	# sttEnd <- c(
 	# 	""
 	# 	, "start"
--- R/foldAllBr.R
+++ R/foldAllBr.R
@@ -6,112 +6,294 @@
 #' @importFrom stringr str_detect
 #' @import rstudioapi
 #' @export
+foldAllBr <- function(time = F){
 
-foldAllBr <- function(){
+fnTmr <- timer(start = T, endOf = "start")
+colFact <- 1E-2
+{
+	# foldBr : given a line, fold the bracket ending it ========================
+	foldBr <- function(posNum){
+		# posNum <- 15.30
+		# opLine <- opBrList[2]
+		savePos <- getPos()
+		# setCursorPosition(document_position(opLine, 999))
+		setCursorPosition(posNum %>% PN_DP)
+		executeCommand("expandToMatching")
+
+		# checking if after expanding
+		brRange <- getPos()
+		brRangePN <- brRange %>% lapply(DP_PN)
+		SPRangePN <- savePos %>% lapply(DP_PN)
+		sttInside <- SPRangePN$start %>% between(brRangePN$start, brRangePN$end)
+		endInside <- SPRangePN$end %>% between(brRangePN$start, brRangePN$end)
+
+		executeCommand("fold")
 
 
-	# R.AlphA::getLibsR.AlphA()
+		# si dedans : on remet au debut de la section qu'on a ferme
+		finalPos <- savePos
+		if(sttInside|endInside) {
+			beforePosNum <- (posNum - colFact)
+			finalPos <- beforePosNum %>% PN_DP
+			message("sttInside : ", sttInside, "\t", "endInside : ", endInside)
+			message("posNum : ", posNum)
+			message("beforePosNum : ", beforePosNum)
+		}
+		message("finalPos : ", finalPos)
+		setCursorPosition(finalPos)
+		# return(getPos()) # inutile ce truc ?
+	}
+
+
+	# foldBrLine : given a line, fold the bracket ending it ====================
+	foldBrLine <- function(opLine){
+		# opLine <- opBrList[2]
+		setCursorPosition(document_position(opLine, 999))
+		executeCommand("expandToMatching")
+		executeCommand("fold")
+		# return(getPos())
+	}
+
+
+	# R.AlphA.Base::getLibsR.AlphA()
+	# getPos : get pos =========================================================
 	getPos <- function(){
 		getSourceEditorContext() %>%
 			primary_selection() %>%
 			getElement("range")
 	}
 
-	retainPos <- getPos()
+	# PN_DP : pos num to document pos ==========================================
+	PN_DP <- function(posNum){
+		row <- floor(posNum)
+		col <- ((posNum - row) / colFact) %>% round(8)
+		document_position(row, col)
+	}
+	# DP_PN : document pos to pos num ==========================================
+	DP_PN <- function(docPos) {
+		posNum <- docPos[1] + (docPos[2] * colFact) %>% round(8)
+		as.numeric(posNum)
+	}
+
+	# DR_PN : document range to pos num ========================================
+	DR_PN <- function(docPos) {
+		posNum <- docPos$start[1] + (docPos$start[2] * colFact) %>% round(8)
+		as.numeric(posNum)
+	}
+
+	PN_DR <- function(posNum_start, posNum_end = posNum_start){
+		SRow <- floor(posNum_start)
+		SCol <- ((posNum_start - SRow) / colFact) %>% round(8)
+		ERow <- floor(posNum_end)
+		ECol <- ((posNum_end - ERow) / colFact) %>% round(8)
+		document_range(
+			start = document_position(SRow, SCol)
+			, end = document_position(ERow, ECol)
+			)
+	}
+
+	# endLine : endLine of a posNum =========
+	endLine <- function(posNum){
+		ceiling(posNum) - colFact
+	}
+
+
+	# nchar for all three types : ------------------------------------------
+	nchars <- function(x, ...){
+		vapply(
+			c("chars", "bytes", "width")
+			, function(tp) nchar(x, tp, ...)
+			, integer(length(x))
+		)
+	}
+
+	# a <- nchars("\u200b")  # in R versions (>= 2015-09-xx):
+	# a <- nchars("\u200b" %>% encodeString)  # in R versions (>= 2015-09-xx):
+	# a <- nchars("\n")
+	# a <- nchars("\n" %>% encodeString)
+	# a <- nchars("\n1234567\n" %>% encodeString)
+	# ## chars bytes width
+	# ##     1     3     0
+
+
+} # local funs
 {
 	retainPos <- getPos()
-} # test
+	# retainPos <- PN_DR(1.1) # for tests
+	# setCursorPosition(1.1 %>% PN_DP)
+	curPosNum <- retainPos$start %>% DP_PN
 	retainStartRow <- retainPos$start[1]
-
-
+	retainStartCol <- retainPos$start[2]
 	docContent <- getSourceEditorContext()$contents %>%
 		data.frame(content = .) %>%
 		tibble::rowid_to_column()
-	opBrList <- docContent %>%
-		filter(content %>% str_detect("(?<!\t.{0,80})\\{$")) %>%
-		# print %>%
-		pull(rowid)
+	fnTmr <- timer(fnTmr, endOf = "read Content")
+} # read content, get current position
 
-	clBrList <- docContent %>%
-		filter(content %>% str_detect("^\\}"))
+fnTmr <- timer(fnTmr, endOf = "init, funs")
+{
 
-	BrList <- docContent %>%
+
+
+
+	opName <- "+"
+	clName <- "-"
+	# opBrPatt <- "(?<!\t.{0,80})\\{$"
+	# clBrPatt <- "^\\}"
+	opBrPatt <- "\\{$" # new one
+	# clBrPatt <- "(^|\t)+\\}" # new one
+	clBrPatt <- "^\t*\\}" # again : only tabs before the bracket
+	comPatt <- "^( |\t)*#.*"
+
+	# for tests only - en fait pas tant que ca ?
+	docContentRet <- docContent %>%
+		mutate(content = content %>% str_remove(comPatt)) %>%
 		mutate(
-			opBr = content %>% str_detect("(?<!\t.{0,80})\\{$")
-			, clBr = content %>% str_detect("^\\}")
+			opBr = content %>% str_detect(opBrPatt)
+			, clBr = content %>% str_detect(clBrPatt)
 			, anyBr = pmax(opBr, clBr)
-			, brTag = paste0(ifelse(opBr, "open", ""), ifelse(clBr, "close", ""))
+			, brTag = paste0(ifelse(opBr, opName, ""), ifelse(clBr, clName, ""))
 		) %>%
-		filter(anyBr == 1) %>%
-		mutate(brPairNb = countSwitches(brTag, "open", "close")) %>%
-		mutate(expected = ceiling(1:n() / 2)) %>%
-		as_tibble %>%
+
+		# filter(!isComment) %>%
 		identity
+	fnTmr <- timer(fnTmr, endOf = "tags")
 
+	docContentRet <- docContentRet %>%
+		countSwitches("brTag", opName, clName) %>%
+		identity
+	fnTmr <- timer(fnTmr, endOf = "countSwitches")
 
-	# # get the 2 surrounding br infos
-	# surrndInfo <- BrList %>%
-	# 	mutate(
-	# 		posDist = rowid - retainStartRow
-	# 		, absPosDist = abs(posDist)
-	# 		, signPosDist = sign(posDist)
-	# 	) %>%
-	# 	group_by(signPosDist) %>%
-	# 	slice_min(absPosDist)
-	# retainPos
-	# surrndInfo
-	# surrndInfo$brTag %>% c("open", "close")
+	docContentRet <- docContentRet %>%
+		# filter(anyBr == 1) %>%
+		# mutate(brPairNb = countSwitches(brTag, opName, clName)) %>%
+		# mutate(expected = ceiling(1:n() / 2)) %>%
+		as_tibble %>%
+		select(-matches("^(brut|inc|check)")) %>%
+		mutate(conCat = paste("0", ret1, ret2, ret3, sep = "_")) %>%
+		mutate(conCatLim = conCat %>% str_remove_all("_0") %>% paste0("_")) %>%
+		mutate(isCur = ifelse(retainStartRow == rowid, "=cur=", "_")) %>%
+		# mutate(isCur = isCur * 100) %>%
+		group_by(conCatLim) %>%
+		mutate(isSecStart = rowid == min(rowid)) %>%
+		ungroup %>%
+		mutate(opBrPlace = content %>% str_extract(paste0(".*", opBrPatt)) %>% nchar) %>%
+		# mutate(opBrPlace = ifelse(rowid == 1, 1, opBrPlace)) %>% # pourquoi ??
+		mutate(opBrPN = rowid + opBrPlace * colFact) %>%
+		# mutate(opBrPN = opBrPN * 100) %>%
+		# print %>%
+		identity
+	fnTmr <- timer(fnTmr, endOf = "ret : other treatments")
+} # back to docContent normal
 
-	lastBr <- BrList %>%
-		mutate(posDist = rowid - retainStartRow) %>%
-		filter(posDist <0) %>% slice_max(posDist)
+{
 
+	curLine <- docContentRet %>% filter(isCur == "=cur=")
+	curPosSec <- curLine$conCatLim # init before check
+	curPosCat <- curLine$catLvl
+	# message("curPosSec : ", curPosSec)
+	noBracket <- !str_detect(curLine$content, opBrPatt)
+	skipIf <- curPosCat == 0 & noBracket
+	if(skipIf) message("noBr - 0")
+	if(curLine$isSecStart & !skipIf){
 
-
-
-	foldBr <- function(opLine){
-		# opLine <- opBrList[2]
-		setCursorPosition(document_position(opLine, 999))
-		executeCommand("expandToMatching")
-		executeCommand("fold")
-		return(getPos())
-	}
-
-
-	if (nrow(lastBr)) { if(lastBr$opBr) {
-		foldBr(lastBr$rowid)
-		# message("this should be printed for single folding")
-		return(NULL) # stop function if this is done
-		message("this should not be printed")
-	}}
-
-	# if last br is not an opened one then close them all
-	# fold Br and list their positions
-	ClosedBr <- opBrList %>% lapply(foldBr)
-
-	# fun to check if current range is in tested range
-	isInRange <- function(current, tested){
-		# tested <- ClosedBr[[1]]
-		# current <- retainPos
-		currentLine <- current$start[1]
-		rowStart <- tested$start[1]
-		rowEnd <- tested$end[1]
-		isInside <- currentLine %>% between(rowStart, rowEnd)
-		if (isInside) {
-			return(rowStart)
+		message("test")
+		# brPlace <- curLine$content %>%
+		# 	str_extract(paste0(".*",opBrPatt)) %>%
+		# 	nchar
+		isBfSecStart <- curLine$opBrPN >= curPosNum
+		isAtSecStart <- curLine$opBrPN == (curPosNum - colFact) %>% round(10)
+		if(isBfSecStart){
+			# message("sec not started yet")
+			curPosSec <- curLine$conCatLim %>% str_remove("_[0-9]*_$")
+			curPosCat <- curLine$catLvl - 1
+		}
+		if(isAtSecStart){
+			executeCommand("expandToMatching")
+			executeCommand("fold")
+			setCursorPosition((curPosNum - colFact) %>% PN_DP)
+			return(NULL)
 		}
 	}
+	# message("curPosCat : ", curPosCat)
 
-	# get the intersections between current range and the closed Br
-	intersections <- ClosedBr %>%
-		lapply(isInRange, current = retainPos) %>%
-		unlist
+	# docContentRet %>% filter(rowid %in% c(300:400, 728:735)) %>% print(n = 150)
+	curSection <- docContentRet %>%
+		filter(conCatLim  %>% str_detect(paste0("^",curPosSec))) %>%
+		identity
+	subSections <- curSection %>%
+		filter(catLvl == curPosCat + 1) %>%
+		identity
+	subSectionsStarts <- subSections %>%
+		group_by(conCatLim) %>%
+		slice_min(rowid) %>%
+		arrange(rowid) %>%
+		identity
 
-	# get cursor back to start (or adjust if there are intersections)
-	if (intersections %>% is.null){
-		setCursorPosition(retainPos) # back
-	} else {
-		newPos <- document_position(intersections %>% min, 1)
-		setCursorPosition(newPos)
-	}
+	fnTmr <- timer(fnTmr, endOf = "docCont norm")
+
+} # back to docContent normal
+{
+	onlyOneSec <- F
+	docContentRet %>% count(ret1, ret2, ret3)
+	if(max(docContentRet$ret1) == 1) onlyOneSec <- TRUE
+	fnTmr <- timer(fnTmr, endOf = "check if 1 big")
+} # check if only 1 big section
+{
+	sectionStartLine <- docContentRet %>%
+		filter(conCatLim == curPosSec) %>%
+		slice_min(rowid)
+
+	sectionStart_PN <- sectionStartLine %>%
+		pull(opBrPN) %>%
+		magrittr::add(colFact)
+	if(skipIf) sectionStart_PN <- (1 + colFact) %>% round(8)
+	fnTmr <- timer(fnTmr, endOf = "sectionStart line and PN")
+
+} # secStart line and PN
+
+subSectionsStarts %>% pull(rowid) %>% lapply(foldBrLine) # fold lines
+fnTmr <- timer(fnTmr, endOf = "fold")
+
+sectionStart_DP <- sectionStart_PN %>% PN_DP
+backToInit <- (onlyOneSec & curPosSec == "0_1")|curPosSec == 0
+{
+	# message("sectionStart_DP : ")
+	# print(sectionStart_DP)
+	# message("onlyOneSec : ", onlyOneSec)
+	# message("curPosSec : ", curPosSec)
+	# message("backToInit : ", backToInit)
+} # messages
+if(backToInit) {
+	setCursorPosition(curPosNum %>% PN_DP)
+} else {
+	setCursorPosition(sectionStart_DP)
+}
+fnTmr <- timer(fnTmr, endOf = "put cursor back - end")
+
+if(time){
+	# popSize <- nrow(old_pop)
+	# timePerM <- sum(fnTmr$dt_seconds/popSize*1E6) %>% round(2)
+	timerPlot <- fnTmr %>%
+		arrange(-heure_seconds) %>%
+		mutate(endOf = factor(endOf, levels = endOf)) %>%
+		# mutate(secsPerMLines = dt_seconds / popSize * 1E6) %>%
+		# mutate(dt100 = (dt_seconds * 100) %>% floor) %>%
+		ggplot(aes(endOf, dt_seconds)) +
+		geom_col() +
+		theme(axis.text = element_text(size = 12)) +
+		geom_text(aes(
+			label = dt_seconds %>% round(2)
+			, y = pmin(dt_seconds + 0.06, 3)
+		)) +
+		coord_flip(ylim = c(0,3)) +
+		ggtitle(
+			paste0("function : ", "foldAllBr")
+			, subtitle = paste0("nrows docContent : ", nrow(docContent))
+		)
+	lum_0_100(50)
+	print(timerPlot)
+} # timer plots
+return(NULL)
+
 }
