700af4c56210c852e80c833f26d61615f3c03bad
adriencocuaud <33268203+adriencocuaud@users.noreply.github.com>
2025-01-20 12:00:02 +0100

    update ret_lum
    add examples and correction of the folowing probleme :
    Error in grDevices::rgb(retLum[1], retLum[2], retLum[3]): l'intensité de couleur 1.5, hors intervalle [0,1]

--- R/ret_lum.R
+++ R/ret_lum.R
@@ -9,13 +9,23 @@
 #' @return A modified hex color in hexadecimal format.
 #' @importFrom grDevices rgb
 #' @importFrom grDevices col2rgb
+#' @examples
+#' # Example 1: Lightening a color
+#' ret_lum("#FF5733", 1.5)  # Returns a lighter version of the input color
+#'
+#' # Example 2: Darkening a color
+#' ret_lum("#FF5733", 0.7)  # Returns a darker version of the input color
 #' @export
 #'
-ret_lum <- function(hexCol, rgbFact){
-	# hexCol <- rgb(1,1,1)
-	# rgbFact <- 0.7
+ret_lum <- function(hexCol, rgbFact) {
+	# Convert hex color to RGB (normalized to [0, 1])
 	back2rgb <- grDevices::col2rgb(hexCol) / 255
-	retLum <- back2rgb*rgbFact
+
+	# Adjust brightness by the factor and clamp values to [0, 1]
+	retLum <- pmax(0, pmin(back2rgb * rgbFact, 1))
+
+	# Convert back to hex format
 	back2Hex <- grDevices::rgb(retLum[1], retLum[2], retLum[3])
 	return(back2Hex)
 }
+
