62f6ac51d8165d34a63535168170d570196f3ada
r-alpha-act <r.alpha.act@outlook.fr>
2025-01-22 22:57:13 +0100

    countSwitches - revise process - update FAB
    - countSwitches : rename collumns - avoid as much as possible column names conflicts, and raise an error if some are found.
    - FoldAllBr : update accordingly

--- R/countSwitches.R
+++ R/countSwitches.R
@@ -20,50 +20,77 @@
 #' @importFrom stats runif
 #' @export
 #'
-countSwitches <- function(data, colNm, sttMark, endMark,
-							  includeStt = TRUE, includeEnd = TRUE){
+countSwitches <- function(
+		data
+		, colNm
+		, sttMark
+		, endMark
+		, includeStt = TRUE
+		, includeEnd = TRUE
+){
+	# R.AlphA_manualRun_start
 	manualrun <- T
 	manualrun <- F
 	if (manualrun) {
-		warning("! parameters manually defined inside function 'countSwitches' ",
-		"for tests. Do not use results !")
+		warning("function 'countSwitches' in manual mode: do not use results.")
 		sttMark <- "+"
 		endMark <- "-"
 		includeStt <- TRUE
 		includeEnd <- TRUE
-		data <- docContentRet
+		# data <- docContent_tags %>% as_tibble %>% print(n = 30)
+		data <- docContent_tags %>% select(brTag) %>% as_tibble
+		data %>%
+			mutate(test = 1- (brTag == "")) %>%
+			mutate(testPart = cumsum(test)) %>%
+			group_by(testPart) %>%
+			mutate(nbLines = n()) %>%
+			distinct(brTag, nbLines)
+		docContent_tags %>% pull(brTag)
+		data <- docContent_tags %>% as_tibble # avec autres colonnes
+		data <- tibble(brTag = c(
+			NULL
+			, rep("", 2)
+			, "+", rep("", 3)
+			, "+", rep("", 1)
+			, "-", rep("", 1)
+			, "+", rep("", 1)
+			, "-", rep("", 1)
+		))
 		colNm <- "brTag"
-	}
-
-	fill_in <- function(prev, new, check) {
-		if_else(check == -1, 0, prev + check)
-	}
-
-	grpTable <- data %>%
-		mutate(stepStr = get(colNm)) %>%
-		as_tibble %>%
-		mutate(findStt = stepStr == sttMark) %>%
-		mutate(findEnd = stepStr == endMark) %>%
-		mutate(nbStt = cumsum(findStt %>% replace_na(0))) %>%
-		mutate(nbEnd = cumsum(findEnd %>% replace_na(0))) %>%
-		mutate(catLvl = nbStt - nbEnd) %>%
-		select(-nbStt, -nbEnd) %>%
-		# spot starts
-		mutate(inc1 = findStt & catLvl == 1) %>%
-		mutate(inc2 = findStt & catLvl == 2) %>%
-		mutate(inc3 = findStt & catLvl == 3) %>%
-		# spot when under category to reset
-		mutate(check1 = ifelse(catLvl >= (1-1), inc1, -1)) %>%
-		mutate(check2 = ifelse(catLvl >= (2-1), inc2, -1)) %>%
-		mutate(check3 = ifelse(catLvl >= (3-1), inc3, -1)) %>%
-		select(-opBr, -clBr, anyBr) %>%
-		mutate(tst1 = cumsum(inc1)) %>%
-		group_by(tst1) %>% mutate(tst2 = cumsum(inc2)) %>%
-		group_by(tst1, tst2) %>% mutate(tst3 = cumsum(inc3)) %>%
-		mutate(ret1 = ifelse(catLvl >= 1, tst1, 0)) %>%
-		mutate(ret2 = ifelse(catLvl >= 2, tst2, 0)) %>%
-		mutate(ret3 = ifelse(catLvl >= 3, tst3, 0)) %>%
-		identity()
-
-	return(grpTable)
+	} # R.AlphA_manualRun
+	{
+		grpTable <- data %>%
+			as_tibble %>%
+			# identify starts and ends
+			mutate(stepStr = get(colNm)) %>%
+			select(stepStr) %>%
+			mutate(findStt = stepStr == sttMark) %>%
+			mutate(findEnd = stepStr == endMark) %>%
+			mutate(nbStt = cumsum(findStt %>% replace_na(0))) %>%
+			mutate(nbEnd = cumsum(findEnd %>% replace_na(0))) %>%
+			mutate(catLvl = nbStt - nbEnd) %>%
+			# attribute starts to levels 1-2-3
+			mutate(inc1 = findStt & catLvl == 1) %>%
+			mutate(inc2 = findStt & catLvl == 2) %>%
+			mutate(inc3 = findStt & catLvl == 3) %>%
+			mutate(raw1 = cumsum(inc1)) %>%
+			group_by(raw1) %>% mutate(raw2 = cumsum(inc2)) %>%
+			group_by(raw1, raw2) %>% mutate(raw3 = cumsum(inc3)) %>%
+			ungroup %>%
+			mutate(lvl_1 = ifelse(catLvl >= 1, raw1, 0)) %>%
+			mutate(lvl_2 = ifelse(catLvl >= 2, raw2, 0)) %>%
+			mutate(lvl_3 = ifelse(catLvl >= 3, raw3, 0)) %>%
+			select(matches("^lvl_[0-9]$"), catLvl) %>%
+			identity
+	} # add lvl infos in a new table --> grpTable
+	{
+		dupCols <- compareVars(grpTable, data)$common
+		if(dupCols %>% length) stop(
+			NULL
+			, "the following columns have been duplicated by countSwitches :\n"
+			, paste0(dupCols, collapse = "\n")
+			, "\n--> please remove them from data before using this function"
+		)
+	} # check for column names conflicts, raising an error in this case
+	return(data %>% bind_cols(grpTable))
 }
