ebe780eef869e1abed8a2bbfdcd47156cf8bfa9c
r-alpha-act <r.alpha.act@outlook.fr>
2025-02-09 20:57:40 +0100

    lum_0_100 - use inputs from Adrien's PR
    check if lum is numeric, and between 0 and 100

--- R/lum_0_100.R
+++ R/lum_0_100.R
@@ -14,10 +14,15 @@
 #' @import ggplot2
 #' @export
 #'
-lum_0_100 <- function(lum){
+lum_0_100 <- function(lum = NULL) {
+
+	if (!is.numeric(lum) || lum < 0 || lum > 100) {
+		stop("lum must be a numeric value between 0 and 100.")
+	}
 
 	lum_pc_leg <- lum/100
 	hex_leg <- rgb(lum_pc_leg,lum_pc_leg,lum_pc_leg)
+
 	dark_adjustments <- theme(
 		plot.background = element_rect(fill = ret_lum(hex_leg,0.7))
 		, legend.background = element_rect(fill = hex_leg)
@@ -27,6 +32,6 @@ lum_0_100 <- function(lum){
 		, axis.text = element_text(color = ret_lum(hex_leg,0.4))
 		, legend.key = element_rect(fill = ret_lum(hex_leg,0.8))
 	)
+
 	theme_set(theme_get()+dark_adjustments)
-	return(NULL)
 }
